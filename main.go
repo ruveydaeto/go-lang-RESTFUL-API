583860627738ae1cb793d7e07ed92814e9e00921
ruveydaeto <ruveydaeto@hotmail.com>
2024-12-26 00:58:54 +0300

    Add initial implementation of book management API with CRUD operations

--- main.go
+++ main.go
@@ -1,37 +1,104 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type book struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
+var books = []book{
+	{
+		ID: "1", Title: "Golang pointers", Author: "Mr. Golang", Quantity: 10,
+	},
+	{
+		ID: "2", Title: "Golang routers", Author: "Mr. Router", Quantity: 20,
+	},
+	{
+		ID: "3", Title: "Golang concurrency", Author: "Mr. Goroutine", Quantity: 30,
+	},
+}
+
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
+}
+
+func bookById(c *gin.Context) {
+	id := c.Param("id")
+	book, err := getBookById(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, book)
+}
+func checkoutBook(c *gin.Context) {
+	id := c.Query("id")
+	if id == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "id is required"})
+		return
+	}
+	book, err := getBookById(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+	if book.Quantity <= 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Book is out of stock"})
+		return
+	}
+	book.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func returnBook(c *gin.Context) {
+	id := c.Query("id")
+	if id == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "id is required"})
+		return
+	}
+	book, err := getBookById(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+		return
+	}
+	book.Quantity += 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func getBookById(id string) (*book, error) {
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+	return nil, errors.New("Book not found")
+}
+
+func createBook(c *gin.Context) {
+	var newBook book //var newBook book ifadesi, book türünde yeni bir değişken oluşturur.
+	if err := c.BindJSON(&newBook); err != nil {
+		return //Eğer JSON verisi geçerli değilse veya bağlama işlemi başarısız olursa, fonksiyon return ifadesi ile sonlanır.
+	}
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook) //HTTP yanıtı olarak 201 Created durum kodu ve yeni oluşturulan kitabın JSON formatında verisini döner.
+
+}
 
 func main() {
-	// var firstName, lastName string = "John", "Doe"
-	// lastName := "Doe" //Bunu kısaltılmış halinde yazsak bile Go dili bunun string olduğunu anlar o yüzden  var firstname string = "John" şeklinde yazmamıza gerek yok
-	// firstName := "John"
-	// var name = "John Doe"
-	// var isMarried bool = true
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.Run("localhost:8080")
+	router.POST("/books", createBook)
+	router.GET("/books/:id", bookById)
+	router.PATCH("/books/checkout", checkoutBook)
+	router.PATCH("/books/return", returnBook)
 
-	// var (
-	// 	age    int     = 40
-	// 	height float32 = 1.75
-	// 	isOkey bool    = true
-	// )
-	// var weight, isStudent = 75, false
-	// isWorking, surname, name := true, "Doe", "John"
-	// fmt.Println(firstName, lastName, isMarried, name, age, height, isOkey, weight, isStudent, isWorking, surname)
-
-	//ZERO VALUES
-	var a int
-	fmt.Println(a) //a değerine bir değer atamadık o yüzden go otomatik zeroa value olarak 0 döner
-	// The zero value is:
-
-	// 0 for numeric types,
-	// false for the boolean type, and
-	// "" (the empty string) for strings.
-
-	studentName := "John"
-	// studentName:= "Doe" //Bu şekilde tekrar tanımlama yapamayız çünkü ":" işareti atama anlamına gelir ikincisinide sadece = kullan yani;
-	studentName = "Doe"
-	fmt.Println(studentName)
-	// fmt.Printf("%T\n", firstName)
-	// fmt.Printf("%T\n", lastName)
-	// fmt.Printf("%T\n", isMarried)
 }
